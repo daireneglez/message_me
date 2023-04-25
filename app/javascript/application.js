53cc3c87e87389ea7a841a3059c59fb381e72d3f
dairenemiereles <dmeireles@norwex.com>
2023-04-25 16:47:12 -0500

    Add navigation and favicon

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import '@doabit/semantic-ui-sass'
+
+// Loads all Semantic javascripts
+//= require semantic-ui
+
+$(document).on('turbolinks:load', function () {
+    $('.ui.dropdown').dropdown();
+})
