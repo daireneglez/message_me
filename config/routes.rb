969f1be3df49b029d5f4c5748f05c27c10432bda
dairenemiereles <dmeireles@norwex.com>
2023-04-28 16:18:12 -0500

    Add login action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "chatroom#index"
+  root "chatroom#index"
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'signup', to: 'users#new'
 end
